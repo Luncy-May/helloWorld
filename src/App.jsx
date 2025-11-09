1199be64e257b17f60482c851c2c5577433d894e
LUNCY579 <clclchengli@gmail.com>
2025-11-09 15:21:57 -0600

    determining the winner on the board

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,13 @@ function calculateWinner(squares) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -39,6 +46,8 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
